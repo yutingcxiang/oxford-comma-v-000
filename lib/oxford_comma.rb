427dcbf2240c36219f48563366741603bc83fb1d
Christine Xiang <yutingcxiang@gmail.com>
2018-07-06 22:02:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
-  array[-1] = "and " + array[-1]
-  return array.join(", ")
+  if array.count > 2
+    array[-1] = "and " + array[-1]
+    return array.join(", ")
+  elsif array.count == 2 
+    return array.join(" and ")
+  else 
+    return array
+  end
 end
